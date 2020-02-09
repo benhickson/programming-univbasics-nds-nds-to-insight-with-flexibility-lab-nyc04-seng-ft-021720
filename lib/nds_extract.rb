babc43b4f28a9972d18334ddf9ee60ff04e23f52
Ben Hickson <bzhickson@gmail.com>
2020-02-09 22:37:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,8 +1,8 @@
 # Provided, don't edit
 require 'directors_database'
 
-# A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
-# [3,4,5], [6]] => [1,2,3,4,5,6].
+# A method we're giving you. This "flattens"  Arrays of Arrays,
+# so: [[1,2], [3,4,5], [6]] => [1,2,3,4,5,6].
 
 def flatten_a_o_a(aoa)
   result = []
@@ -48,6 +48,14 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  
+  output_array = []
+  
+  movies_collection.length.times do |i|
+    output_array.push(movie_with_director_name(name, movies_collection[i]))
+  end
+  
+  output_array
 end
 
 
@@ -63,6 +71,13 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  output_hash = {}
+  
+  collection.length.times do |i|
+    output_hash[collection[i][:studio]] = collection[i][:worldwide_gross]
+  end
+  
+  output_hash
 end
 
 def movies_with_directors_set(source)
@@ -76,6 +91,9 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  
+  
+  
 end
 
 # ----------------    End of Your Code Region --------------------
